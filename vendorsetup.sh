51010a8ac3b587b85f7e1355ad6aea36bad4da55
Murat Kozan <muratkozan350@gmail.com>
2023-04-21 22:06:25 +0300

    haydn: Build Aperture camera for now
    Leica cam rip

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,32 +1,27 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Device common
-echo 'Cloning common device tree [1/7]'
+echo 'Cloning common device tree [1/6]'
 git clone https://github.com/Astridxx/android_device_xiaomi_sm8350-common -b RisingOS-13 device/xiaomi/sm8350-common
 
 # Kernel
-echo 'Cloning kernel tree [2/7]'
+echo 'Cloning kernel tree [2/6]'
 git clone --depth=1 https://github.com/Astridxx/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
 
 # Vendor
-echo 'Cloning vendor tree [3/7]'
+echo 'Cloning vendor tree [3/6]'
 git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
 
 # Vendor common
-echo 'Cloning common vendor tree [4/7]'
+echo 'Cloning common vendor tree [4/6]'
 git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_sm8350-common -b RisingOS-13 vendor/xiaomi/sm8350-common
 
 # Firmware
-echo 'Cloning firmware [5/7]'
+echo 'Cloning firmware [5/6]'
 git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
 
 # Xiaomi
-echo 'Cloning hardware xiaomi [6/7]'
+echo 'Cloning hardware xiaomi [6/6]'
 rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-
-# Camera
-echo 'Cloning Leica Camera [7/7]'
-git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
-
 
 echo 'delete vendorsetup.sh from device tree once this is done'
