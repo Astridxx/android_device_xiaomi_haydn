08723716fb2ba4ca2f95053e0249eaf23e75fbe0
Astridxx <muratkozan350@gmail.com>
2023-08-24 13:45:28 +0300

    haydn: Pixys-Ify

--- pixys_haydn.mk
+++ pixys_haydn.mk
@@ -11,17 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := pixys_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
+
+# PixysOS
+WITH_GMS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="haydn_global-user 13 TKQ1.220829.002 V14.0.5.0.TKKMIXM release-keys" \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,38 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
+# Device common
+echo 'Cloning common device tree'
+git clone https://github.com/Astridxx/android_device_xiaomi_sm8350-common -b Pixys-13 device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone https://github.com/Astridxx/android_kernel_xiaomi_sm8350.git --recursive --depth=1 -b Octavi-13 kernel/xiaomi/sm8350
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
+
+# Vendor common
+echo 'Cloning common vendor tree'
+git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+
+# Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi && git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+
+# Display
+echo 'Cloning display hal'
+rm -rf hardware/qcom-caf/sm8350/display && git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-sm8350 hardware/qcom-caf/sm8350/display
+
+echo 'Cloning opensource display'
+rm -rf vendor/qcom/opensource/display && git clone --depth=1 https://github.com/LineageOS/android_vendor_qcom_opensource_display -b lineage-20.0 vendor/qcom/opensource/display
+
+echo 'Cloning opensource commonsys display'
+rm -rf vendor/qcom/opensource/commonsys/display && git clone --depth=1 https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys -b lineage-20.0 vendor/qcom/opensource/commonsys/display
+
+echo 'Cloning opensource commonsys-intf display'
+rm -rf vendor/qcom/opensource/commonsys-intf/display && git clone --depth=1 https://github.com/LineageOS/android_vendor_qcom_opensource_display-commonsys-intf -b lineage-20.0 vendor/qcom/opensource/commonsys-intf/display
+
 # Firmware
 echo 'Cloning firmware'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
@@ -7,5 +40,12 @@ git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmwar
 # Camera
 echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
+
+# Faceunlock
+echo 'Fixing faceunlock conflict with vendor libs'
+cd external/faceunlock
+wget https://raw.githubusercontent.com/Astridxx/Patch-Haydn/main/Pixys/0001-Fix-naming-conflict-with-vendor-libs.patch
+patch -p1 <0001-Fix-naming-conflict-with-vendor-libs.patch
+cd ../..
 
 echo 'delete vendorsetup.sh from device tree once this is done'
